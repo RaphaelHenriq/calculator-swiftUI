6e0bfbfe8365f71e905faf4be6b62b4ad3978c7e
Raphael Henrique <45474283+RaphaelHenriq@users.noreply.github.com>
2023-08-16 10:58:34 -0300

    Init project

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Raphael Henrique on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
